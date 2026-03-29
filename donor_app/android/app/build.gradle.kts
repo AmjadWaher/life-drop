7681355f8cb12a7e02cfe2fdf45b6f391710937e
AmjadWaher <amjadwaher623@gmail.com>
2026-03-29 19:28:14 +0300

    - Added FastLane for Android, and connected it to Firebase App Distribution.
    - Bumped version to 1.0.2.

--- donor_app/android/app/build.gradle.kts
+++ donor_app/android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -56,7 +59,6 @@ android {
                 name = "app_name",
                 value = "Life Drop Production"
                 )
-            applicationIdSuffix = ".prod"
         }
     }
 }
