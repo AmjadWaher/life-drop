038ca6227e65dcbccf0d5b8136ccfc528915e54f
AmjadWaher <amjadwaher623@gmail.com>
2026-05-06 23:52:15 +0300

    Separate Auth constants from API constants

--- donor_app/lib/core/networking/api_constants.dart
+++ donor_app/lib/core/networking/api_constants.dart
@@ -1,25 +1,7 @@
 class ApiConstants {
   static const String baseUrl = 'http://192.168.1.221:7894/api';
+
   static const String auth = '$baseUrl/auth';
-  static const String donors = '$baseUrl/Donors';
-
-  //-------------- Auth --------------
-
-  static const String login = '$auth/login';
-  static const String verifyOtp = '$auth/verify-otp';
-  static const String sendOtp = '$auth/forgot-password';
-  static const String resetPassword = '$auth/reset-password';
-  static const String resendOtp = '$auth/resend-otp';
-
-  //-------------- Location --------------
-
-  static const String governorates = '$baseUrl/locations/governorates';
-  static String districts(String governorateId) =>
-      '$governorates/$governorateId/districts';
-
-  //-------------- Donors --------------
-
-  static const String register = '$donors/register';
-  static const String verifyRegistration = '$donors/verify-registration';
-  static const String resendRegistrationOtp = '$donors/resend-registration-otp';
+  static const String donors = '$baseUrl/donors';
+  static const String locations = '$baseUrl/locations';
 }
